b3186968a5e55a6907752181a106e7e39b428109
Dimas Julisa Taniawan <dimazniawan@gmail.com>
2015-10-31 09:14:14 +0700

    first commit

---
+++ lib/spinjs/for/rails.rb
@@ -0,0 +1,9 @@
+require "spinjs/for/rails/version"
+
+module Spinjs
+  module For
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/spinjs/for/rails/version.rb
@@ -0,0 +1,7 @@
+module Spinjs
+  module For
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
